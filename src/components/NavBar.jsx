6c0afc6cd4a0cca68be7b74fa746873f3a507936
optik1 <bearden86@gmail.com>
2024-09-16 18:57:27 -0400

    create navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,12 @@
+import { Link } from 'react-router-dom';
+
 export default function NavBar() {
   return (
     <div>
       <h1>Nav Bar</h1>
+      <Link to="/">Home</Link>
+      <Link to="/players">Players</Link>
+      <Link to="/players/new">New Player</Link>
     </div>
   );
 }
