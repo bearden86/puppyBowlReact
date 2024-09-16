b1e15357a47ab6c2b852520c1b6c6f0b154e8165
optik1 <bearden86@gmail.com>
2024-09-16 19:38:28 -0400

    more changes to singleplayer component

--- src/components/SinglePlayer.jsx
+++ src/components/SinglePlayer.jsx
@@ -1,16 +1,31 @@
-import { useParams } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import { fetchSinglePlayer } from "../API";
 import { useNavigate } from "react-router-dom";
 
 export default function SinglePlayers() {
   const { id } = useParams();
+  const [players, setPlayers] = useState([]);
   const navigate = useNavigate();
   navigate(`/players/${id}`);
-  fetchSinglePlayer(id);
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await fetchSinglePlayer(id);
+      setPlayers(data);
+    }
+    fetchData();
+  }, [id]);
 
   return (
     <div>
-      <h1>Single Players</h1>
+      {players.map((player) => (
+        <div key={player.id}>
+          <h2>{player.name}</h2>
+          <p>{player.breed}</p>
+          <img src={player.imageUrl} alt={player.name} />
+        </div>
+      ))}
     </div>
   );
 }
