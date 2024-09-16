6c5e499bd670f31d99cb74c2b74711bf82ab7a7b
optik1 <bearden86@gmail.com>
2024-09-16 19:18:10 -0400

    added single player button

--- src/components/SinglePlayer.jsx
+++ src/components/SinglePlayer.jsx
@@ -1,4 +1,9 @@
+import { useNavigate } from "react-router-dom";
+
 export default function SinglePlayers() {
+  const navigate = useNavigate();
+  navigate(`/players/${id}`);
+
   return (
     <div>
       <h1>Single Players</h1>
