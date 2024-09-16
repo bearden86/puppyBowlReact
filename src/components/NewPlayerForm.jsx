4ba1bbf39a1fd4fc257e855682198090edc3a283
optik1 <bearden86@gmail.com>
2024-09-16 19:59:02 -0400

    working thru newplayerform

--- src/components/NewPlayerForm.jsx
+++ src/components/NewPlayerForm.jsx
@@ -1,13 +1,29 @@
 import { useNavigate } from "react-router-dom";
 
+//create the form and ajax helper function to create a new player in this component
 
 export default function NewPlayerForm() {
   const navigate = useNavigate();
   navigate(`/players/${id}`);
+  const [name, setName] = useState("John Wayne");
+  const [breed, setBreed] = useState("Golden Retriever");
+  const [status, setStatus] = useState("bench");
+
+  const updateName = (event) => {
+    setName(event.target.value);
+  };
+  const updateBreed = (event) => {
+    setBreed(event.target.value);
+  };
+  const updateStatus = (event) => {
+    setStatus(event.target.value);
+  };
 
   return (
     <div>
-      <h1>New Player Form</h1>
+      <p>Name: {name}</p>
+      <p>Breed: {breed}</p>
+      <p>Status: {status}</p>
     </div>
   );
 }
